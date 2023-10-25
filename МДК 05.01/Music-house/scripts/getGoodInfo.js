a65d22b1ec5527046027683df4429df2718da4f1
Nikita_Makeevsky <74784748+CeCigan@users.noreply.github.com>
2023-10-25 15:28:28 +0500

    Add files via upload
    Обновление от 25.10.23

---
+++ МДК 05.01/Music-house/scripts/getGoodInfo.js
@@ -0,0 +1,12 @@
+let showGoodBtn = document.querySelectorAll('#showGoodBtn'),
+    goodId = 0;
+
+showGoodBtn.addEventListener('click', ()=>{
+    goodId = function setGoodId(id = Number){
+        return id;
+    };
+});
+
+
+console.log(goodId);
+
---
+++ МДК 05.01/Music-house/scripts/showGoodInfo.js
@@ -0,0 +1,3 @@
+import {
+    goodId
+} from 'getGoodInfo.js';
